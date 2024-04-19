9aae32b9e40d57c5f0d2312ba4e392b7ec290fec
Raul Quesada Navarro <raquenaengineering@gmail.com>
2024-04-19 19:12:50 +0200

    - rename ethernet_config to controllino_ethernet_config.

---
+++ src/controllino_ethernet_config.h
@@ -0,0 +1,27 @@
+#ifndef ETHERNET_CONFIG_H
+#define ETHERNET_CONFIG_H
+
+# include "Ethernet.h"
+
+// byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0x09, 0x70 };      // should be written on the back side of the controllino. 
+// IPAddress ip(192,168,4,201);                              // ip of your choice. 
+// IPAddress gateway(192,168,1, 1);
+// IPAddress subnet(255, 255, 0, 0);
+
+// ETHERNET CONFIGURATION ///////////////////////////////////////////////
+
+#define CONTROLLINO_IO_PORT 8881
+
+
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+
+IPAddress local_ip(192, 168, 4, 128);
+IPAddress gateway(192, 168, 4, 1);
+IPAddress subnet(255, 255, 0, 0);
+
+IPAddress primaryDNS(8, 8, 8, 8); 
+IPAddress secondaryDNS(8, 8, 4, 4);
+
+
+ 
+#endif
